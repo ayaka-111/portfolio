33dd3fafa7e6986d0a357de4b5a70f54958d5cf6
ayaka <98952604+ayaka-111@users.noreply.github.com>
2022-03-26 12:56:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 function toggleNav() {
     var body = document.body;
     var hamburger = document.getElementById('js-menu');
-    var blackBg = document.getElementById('js-white-bg');
+    var whiteBg = document.getElementById('js-white-bg');
   
     hamburger.addEventListener('click', function() {
       body.classList.toggle('nav-open');
     });
-    blackBg.addEventListener('click', function() {
+    whiteBg.addEventListener('click', function() {
       body.classList.remove('nav-open');
     });
   }
